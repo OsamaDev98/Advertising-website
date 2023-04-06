b29f87936ab4cc29ed190f34cd6ac366702bc5bb
osama <osamadev921@gmail.com>
2023-04-06 02:42:13 +0200

    update new"

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Home />
+      {/* <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </Router>
+      </Router> */}
     </div>
   );
 }
